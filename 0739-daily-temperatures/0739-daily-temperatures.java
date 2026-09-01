993bc87af530c25d5dfbe55c1d87eb5bae6e1fd5
vtu29731 <vtu29731@veltech.edu.in>
2026-09-01 20:42:27 +0530

    Time: 60 ms (78.46%), Space: 107.9 MB (14.85%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() &&
+                   temperatures[i] > temperatures[stack.peek()]) {
+                int previousDay = stack.pop();
+                answer[previousDay] = i - previousDay;
+            }
+            stack.push(i);
+        }
+        return answer;
+    }
+}
